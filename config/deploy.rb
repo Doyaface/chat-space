09a07fab7b2c650eb1eca646012cfcdfc1307da5
Doyaface <takahiro@babatakashidainoMacBook-puro.local>
2019-04-27 16:50:00 +0900

    deploy_fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/bt0222.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid"}
-set :unicorn_config_path, -> ( "#{current_path}/config/unicorn.rb")
+set :unicorn_config_path, -> {"#{current_path}/config/unicorn.rb"}
 set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
